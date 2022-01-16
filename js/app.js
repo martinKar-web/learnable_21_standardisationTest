a35aba6210ea0be48b8a7491d9df73a76525b952
Martin Karugaba <martinkarugaba21@gmail.com>
2022-01-16 22:58:27 +0300

    solved responsive nav z-index issue

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.querySelector('.nav-bar');
+
+hamburger.addEventListener('click', () => {
+  navbar.classList.toggle('open');
+})
